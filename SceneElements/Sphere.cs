2eb26dadb72821d93d4501e43a574469bd39d88c
Karel Boerstoel <k.boersto@gmail.com>
2024-05-03 00:01:10 +0200

    added the plane, sphere and triangle primitives

--- SceneElements/Sphere.cs
+++ SceneElements/Sphere.cs
@@ -5,10 +5,36 @@ namespace OpenTK.SceneElements;
 public class Sphere : IPrimitive
 {
     public Vector3 Center { get; set; }
+    public float Radius { get; set; }
     public Color4 Color { get; set; }
     public Material Material { get; set; }
+    public Sphere(Vector3 center, float radius, Color4 color, Material material)
+    {
+        Center = center;
+        Radius = radius;
+        Color = color;
+        Material = material;
+    }
+
+    //only works if ray.Direction is normalised (should be the case)
     public float RayIntersect(Ray ray)
     {
-        throw new NotImplementedException();
+        //note that the a in the discriminant is always 1 as the ray is normalised
+        Vector3 V = ray.Origin - Center;
+        float b = 2 * (V.X + V.Y + V.Z);
+        float c = MathF.Pow(ray.Origin.X + Center.X, 2) + MathF.Pow(ray.Origin.X + Center.X, 2) + MathF.Pow(ray.Origin.X + Center.X, 2) - MathF.Pow(Radius, 2);
+        float discriminant = MathF.Pow(b, 2) + 4 * c;
+        if (discriminant < 0)
+            return float.MinValue;
+        float rootOfDiscriminant = MathF.Sqrt(discriminant);
+        float t1 = (-b + rootOfDiscriminant) / 2;
+        float t2 = (-b - rootOfDiscriminant) / 2;
+        if(t1 <= 0)
+            return float.MinValue;
+        if (t2 <= 0)
+            return t1;
+        return t2;
+
+
     }
 }
---
+++ SceneElements/Triangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using OpenTK.Mathematics;
+using OpenTK.SceneElements;
+
+namespace INFOGR2024Template.SceneElements
+{
+    internal class Triangle : IPrimitive
+    {
+        public Vector3 Center { get; set; }
+        public Vector3 Normal { get; private set; }
+        public Vector3 VectorA { get; private set; }
+        public Vector3 VectorB { get; private set; }
+        public Vector3 VectorC { get; private set; }
+        public Vector3 PointA => Center + VectorA;
+        public Vector3 PointB => Center + VectorB;
+        public Vector3 PointC => Center + VectorC;
+        public Color4 Color { get; set; }
+        public Material Material { get; set; }
+
+        public Triangle(Vector3 PointA, Vector3 PointB, Vector3 PointC, Color4 color, Material material)
+        {
+            Center = (PointA + PointB + PointC) / 3;
+            VectorA = PointA - Center;
+            VectorB = PointB - Center;
+            VectorC = PointC - Center;
+            Vector3 cross = Vector3.Cross(PointB - PointA, PointC - PointA);
+            Normal = cross / cross.Length;
+            Color = color;
+            Material = material;
+        }
+        /*public Triangle(Vector3 center, Vector3 vecA, Vector3 vecB, Vector3 vecC, Color4 color, Material material)
+        {
+            to be added
+        }*/
+        /*public Triangle(Vector3 center, Vector3 normal, float halfDimension, Color4 color, Material material)
+        {   
+            to be added
+        }*/
+        public float RayIntersect(Ray ray)
+        {
+            float denominator = Vector3.Dot(ray.Direction, Normal);
+            //if denominator is 0 then the vector is parallel and the ray will not hit.
+            if (denominator == 0)
+                return float.MinValue;
+            float t = Vector3.Dot(Center - ray.Origin, Normal) / denominator;
+            //the intersection can be ignored if it is behind the camera
+            if (t <= 0)
+                return float.MinValue;
+            Vector3 intersection = ray.Origin + t * ray.Direction;
+            //the intersection can be ignored if the point lies outside the triangle
+            if (Vector3.Dot(Vector3.Cross(PointB - PointA, intersection - PointA), Normal) < 0 
+                || Vector3.Dot(Vector3.Cross(PointB - PointA, intersection - PointA), Normal) < 0
+                || Vector3.Dot(Vector3.Cross(PointB - PointA, intersection - PointA), Normal) < 0)
+                return float.MinValue;
+            return t;
+        }
+    }
+}
