f35ebaf7595ec7ee6a12f587ff2741537c853d6d
Phlipsje <144818518+Phlipsje@users.noreply.github.com>
2024-05-02 17:44:16 +0200

    Made base camera

--- SceneElements/Camera.cs
+++ SceneElements/Camera.cs
@@ -1,3 +1,5 @@
+using OpenTK.Mathematics;
+
 namespace OpenTK.SceneElements;
 
 /// <summary>
@@ -5,5 +7,36 @@ namespace OpenTK.SceneElements;
 /// </summary>
 public class Camera
 {
+    public Vector3 Position;
+    public float DistanceToCenter;
+    public int Width;
+    public int Height;
+    public Vector3 ViewDirection; //Unit vector, through center of camera plain
+    //Rotate around X axis
+    public Vector3 UpDirection => new(0, -ViewDirection.X, ViewDirection.Y); //Unit vector
+    //Rotate around Y axis
+    public Vector3 RightDirection => new(ViewDirection.Z, ViewDirection.Y, -ViewDirection.X); //Unit vector
+    public Vector3 ImagePlaneCenter => Position + DistanceToCenter * ViewDirection;
+    public float FieldOfView => MathF.Acos(Height / 2 / DistanceToCenter) * 2;
+    public float AspectRatio => Width / (float)Height;
+
+    //This will automatically focus on an object on (0,0,0)
+    public Camera()
+    {
+        Position = new Vector3(-2, 2, -2);
+        DistanceToCenter = 1f;
+        Width = 1280;
+        Height = 720;
+        ViewDirection = new Vector3(0.5f, -0.5f, 0.5f);
+    }
     
+    //Custom values
+    public Camera(Vector3 position, Vector3 viewDirection, float distanceToCenter, int width, int height)
+    {
+        Position = position;
+        ViewDirection = viewDirection;
+        DistanceToCenter = distanceToCenter;
+        Width = width;
+        Height = height;
+    }
 }
