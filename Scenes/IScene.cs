c15d40cdb6af0a95a3ae009391e631f84b7ab94d
kaan177 <66557991+kaan177@users.noreply.github.com>
2024-05-03 13:34:46 +0200

    fixed scene interface properties

--- Scenes/IScene.cs
+++ Scenes/IScene.cs
@@ -12,8 +12,8 @@ namespace INFOGR2024Template.Scenes
     /// </summary>
     public interface IScene
     {
-        public List<IPrimitive> primitives { get; }
-        public Camera camera { get; }
+        public List<IPrimitive> primitives { get; set; }
+        public Camera camera { get; set; }
 
         public void Tick();
     }
