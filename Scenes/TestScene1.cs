b0f56583b47cfffcc5378604dd391c1c4971620d
kaan177 <66557991+kaan177@users.noreply.github.com>
2024-05-03 14:22:43 +0200

    added objects to scene1

--- Scenes/TestScene1.cs
+++ Scenes/TestScene1.cs
@@ -1,4 +1,5 @@
 ﻿using OpenTK.SceneElements;
+using OpenTK.Mathematics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,11 @@ namespace INFOGR2024Template.Scenes
 
         public TestScene1() 
         {
-            primitives = new List<IPrimitive> { };
+            primitives = new List<IPrimitive> 
+            {
+                new Sphere(new Vector3(1,1,1), 1, new Material(new Color4(1,1,1,1))),
+                new Sphere(new Vector3(2,2,2), 0.2f, new Material(new Color4(1,0,0,1)))
+            };
             camera = new Camera();
         }
         public void Tick()
