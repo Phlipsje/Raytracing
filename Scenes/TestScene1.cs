eba284c12b866988ebd872bb3447c68e1dd5097c
kaan177 <66557991+kaan177@users.noreply.github.com>
2024-05-03 13:35:15 +0200

    created test scene 1

---
+++ Scenes/TestScene1.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.SceneElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFOGR2024Template.Scenes
+{
+    public class TestScene1 : IScene
+    {
+        public List<IPrimitive> primitives { get; set; }
+        public Camera camera {  get; set; }
+
+        public TestScene1() 
+        {
+            primitives = new List<IPrimitive> { };
+            camera = new Camera();
+        }
+        public void Tick()
+        {
+            
+        }
+    }
+}
