a5e87685e2c8c942cae39361e2147e0fcf57f5de
Karel Boerstoel <k.boersto@gmail.com>
2024-05-06 15:04:05 +0200

    fixed some bugs

--- Scenes/TestScene1.cs
+++ Scenes/TestScene1.cs
@@ -20,22 +20,22 @@ namespace INFOGR2024Template.Scenes
         public TestScene1() 
         {
             Camera = new Camera(new Vector3(1, 5f, -5), new Vector3(0f, -1f, 1f), new Vector3(1, 0f, 0), 1f, 1.6f, 0.9f);
+            //Camera = new Camera(new Vector3(0, 1f, -5), new Vector3(0f, 0f, 1f), new Vector3(1f, 0f, 0), 1f, 1.6f, 0.9f);
             Primitives = new List<IPrimitive>
             {
                 /*new Sphere(new Vector3(1,1,1), 1, new Material(new Color4(1,1,1,1))),
-                new Sphere(new Vector3(2,2,2), 0.2f, new Material(new Color4(1,0,0,1)))*/
-                
+                new Sphere(new Vector3(2,2,2), 0.2f, new Material(new Color4(1,0,0,1)))*/        
                 new Sphere(new Vector3(-1, 0.5f, 0), 0.5f, new Material(Color4.Red)),
                 new Plane(new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Material(Color4.White))
             };
-            Primitives = Primitives.Concat(OBJImportHelper.ImportModel(OBJImportHelper.FilePath("cube"), 0.02f, new Vector3(3, 0, 1), new Material(Color4.Yellow))).ToList();
-            Primitives = Primitives.Concat(OBJImportHelper.ImportModel(OBJImportHelper.FilePath("pyramid"), 0.03f, new Vector3(1, 0, 0), new Material(Color4.Blue))).ToList();
+            /*Primitives = Primitives.Concat(OBJImportHelper.ImportModel(OBJImportHelper.FilePath("cube"), 0.02f, new Vector3(3, 0, 1), new Material(Color4.Yellow))).ToList();
+            Primitives = Primitives.Concat(OBJImportHelper.ImportModel(OBJImportHelper.FilePath("pyramid"), 0.03f, new Vector3(1, 0, 0), new Material(Color4.Blue))).ToList();*/
             PointLights = new List<Vector3>
             {
-                new Vector3(-10f, 5f, 0f), 
-                new Vector3(0f, 5f, 10f), 
+                //new Vector3(-10f, 5f, 0f), 
+                new Vector3(0f, 5f, -10f), 
                 new Vector3(-10f, 10f, 5f), 
-                new Vector3(5f, 10f, 5f)
+                new Vector3(0f, 1f, 0f)
             };
         }
         public void Tick()
